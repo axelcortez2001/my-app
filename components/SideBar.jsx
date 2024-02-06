521b5f99520e4c2b5006481b23c38b198c17987e
Axel Cortez <johnaxelcortez21@gmail.com>
2024-02-06 15:54:01 +1100

    initial commit

---
+++ components/SideBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SideBar() {
+  return (
+    <div className='h-screen flex justify-start w-80 border'>
+      <h1>Profile Name</h1>
+    </div>
+  );
+}
